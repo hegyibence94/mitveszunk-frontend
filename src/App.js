3cd414024f70fd8dbeec9153cef3a44fa7f6c24c
Bence Hegyi <hegyibence94@gmail.com>
2023-11-05 10:26:41 +0100

    Mari cuki appja

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Mari is cuki.
+          Mari very cuki mit(v)eszünk appja.
         </p>
         <a
           className="App-link"
